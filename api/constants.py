812cf9c3a2b569adb218f2ae5712e9603cbe9153
Andreas Lubojanski <andreas.lubojanski@hs-nordhausen.de>
2023-05-31 11:43:40 +0200

    removed unneccessary statements

--- api/constants.py
+++ api/constants.py
@@ -7,4 +7,3 @@ FTP_SERVER = os.getenv("FTP_SERVER", "csdata.tu-ilmenau.de")
 
 LICENSE_PATH = os.getenv("GUROBI_LICENSE_FILE_PATH", "/home/pyrokar/gurobi_docker.lic")
 LOCAL_STORAGE_DIR = os.path.abspath(os.getenv("LOCAL_STORAGE_DIR", "/home/pyrokar/github/inretensys-common/data/working"))
-print(LOCAL_STORAGE_DIR)
